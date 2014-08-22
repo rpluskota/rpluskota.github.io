3f193790176c029063b68438e010aaecb4c1c3b2
rpluskota <plurad1703@gmail.com>
2014-08-22 23:54:50 +0200

    test

--- resources/js/pages.js
+++ resources/js/pages.js
@@ -1,10 +1,30 @@
-function doSomething() {
+function goAbout() {
  $(document).ready(function(){
-				$('#content').load('index.html');
+				$('#content').load('pages/about.html');
 			});
 };
-function doSomethingelse() {
+function go2d() {
  $(document).ready(function(){
-				$('#content').load('index2.html');
+				$('#content').load('pages/2d.html');
+			});
+};
+function goPoly() {
+ $(document).ready(function(){
+				$('#content').load('pages/poly.html');
+			});
+};
+function go3d() {
+ $(document).ready(function(){
+				$('#content').load('pages/3d.html');
+			});
+};
+function govis() {
+ $(document).ready(function(){
+				$('#content').load('pages/vis.html');
+			});
+};
+function goContact() {
+ $(document).ready(function(){
+				$('#content').load('pages/contact.html');
 			});
 }
