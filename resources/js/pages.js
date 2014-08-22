aa3329051dad00322801df3d08f4c3e0e0cf9c9a
rpluskota <plurad1703@gmail.com>
2014-08-22 23:31:32 +0200

    test

---
+++ resources/js/pages.js
@@ -0,0 +1,10 @@
+function doSomething() {
+ $(document).ready(function(){
+				$('#content').load('index.html');
+			});
+};
+function doSomethingelse() {
+ $(document).ready(function(){
+				$('#content').load('index2.html');
+			});
+}
