29b6e4a76f991bb276a530140cafc938bdbdcce2
KotKot <mr-ed@o2.pl>
2014-08-27 14:16:10 +0200

    test

---
+++ resources/js/visualizations.js
@@ -0,0 +1,15 @@
+function showV1() {
+ $(document).ready(function(){
+				$('#viscontainer').load('visual/v1.html');
+			});
+};
+function showV2() {
+ $(document).ready(function(){
+				$('#viscontainer').load('pages/v2.html');
+			});
+};
+function showV3() {
+ $(document).ready(function(){
+				$('#viscontainer').load('pages/v3.html');
+			});
+};
